7c1337ea79750835b3b0f96d67ae1ea0a33c78e9
hoale240803 <hoale240803@gmail.com>
2024-04-30 17:05:13 +0700

    sns - create consumer

--- 4.Sns/SnsConsumer/Program.cs
+++ 4.Sns/SnsConsumer/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+
+var queueName = args.Length == 1 ? "demo_queue" : "demo_sqs2";
+
+var cts = new CancellationTokenSource();
+var sqsClient = new AmazonSQSClient();
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync(queueName);
+
+var receiveMessageRequest = new ReceiveMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    AttributeNames = new List<string> { "All" },
+    MessageAttributeNames = new List<string> { "All" }
+};
+
+while (!cts.IsCancellationRequested)
+{
+    var response = await sqsClient.ReceiveMessageAsync(receiveMessageRequest, cts.Token);
+
+    foreach (var message in response.Messages)
+    {
+        Console.WriteLine($"Message Id: {message.MessageId}");
+        Console.WriteLine($"Message Body: {message.Body}");
+
+        await sqsClient.DeleteMessageAsync(queueUrlResponse.QueueUrl, message.ReceiptHandle);
+    }
+    await Task.Delay(500);
+}
+
+
+
+
